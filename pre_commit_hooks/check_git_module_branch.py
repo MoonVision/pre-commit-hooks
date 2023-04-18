afe801c755a91a0a34948724c68a77000ea55522
Jakob Klepp <jakob.klepp@moonvision.io>
2023-04-18 11:26:15 +0200

    Pass linting

--- pre_commit_hooks/check_git_module_branch.py
+++ pre_commit_hooks/check_git_module_branch.py
@@ -1,30 +1,38 @@
+from __future__ import annotations
+
 import argparse
 import dataclasses
 import os
 import re
 from pathlib import Path
-from typing import Sequence, Iterator, Optional, Dict
+from typing import Iterator
+from typing import Sequence
 
 from pre_commit_hooks.util import cmd_output
 
 
-def parse_gitmodules_properties(gitmodules_text: str) -> Optional[Dict[str, Dict[str, str]]]:
+def parse_gitmodules_properties(
+    gitmodules_text: str,
+) -> dict[str, dict[str, str]] | None:
     name_re = re.compile(r"\[submodule \"(?P<name>[^\"]+)\"]")
-    properties = dict()
+    properties: dict[str, dict[str, str]] = dict()
 
     sm_name = None
     for line in gitmodules_text.splitlines():
         name_match = name_re.match(line)
         if name_match:
-            sm_name = name_match.group("name")
+            sm_name = name_match.group('name')
             properties[sm_name] = dict()
         else:
-            split = line.split("=", maxsplit=1)
+            split = line.split('=', maxsplit=1)
             if len(split) == 2:
                 key_raw, value_raw = split
                 key = key_raw.strip()
                 value = value_raw.strip()
-                properties[sm_name][key] = value
+                if sm_name and key:
+                    properties[sm_name][key] = value
+                else:
+                    raise Exception('.gitmodules malformed')
     return properties
 
 
@@ -35,30 +43,34 @@ class DiffLine:
     sha1_src: str
     sha1_dst: str
     status: str
-    status_score: Optional[int]
+    status_score: int | None
     src: str
-    dst: Optional[str]
+    dst: str | None
 
 
-def get_diff_data(from_ref: Optional[str], to_ref: Optional[str], filenames: list[str]) -> Iterator[DiffLine]:
+def get_diff_data(
+    from_ref: str | None,
+    to_ref: str | None,
+    filenames: list[str],
+) -> Iterator[DiffLine]:
     if from_ref and to_ref:
-        diff_arg = f"{from_ref}...{to_ref}"
+        diff_arg = f'{from_ref}...{to_ref}'
     else:
-        diff_arg = "--staged"
+        diff_arg = '--staged'
 
     diff_out = cmd_output(
-        "git", "diff", "--diff-filter=A", "--raw", diff_arg, "--",
+        'git', 'diff', '--diff-filter=A', '--raw', diff_arg, '--',
         *filenames,
     )
 
     for line in diff_out.splitlines():
-        fields = line.split("\t", 1)
+        fields = line.split('\t', 1)
         modes = fields[0]
         src = fields[1]
         dst = None
         if len(fields) >= 2:
             dst = fields[2]
-        mode_src = modes[0].replace(":", "")
+        mode_src = modes[0].replace(':', '')
         status_score = None
         if len(modes[4]) > 1:
             status_score = int(modes[4][1:])
@@ -74,36 +86,42 @@ def get_diff_data(from_ref: Optional[str], to_ref: Optional[str], filenames: lis
         )
 
 
-def update_branch_prop_in_gitmodules_text(gitmodules_text: str, mod_name: str, new_branch: str) -> str:
-    mod_branch_re = re.compile(fr"(\[submodule \"{mod_name}\"](\s[^\[]+)+branch\s?=\s?)(.+)")
+def update_branch_prop_in_gitmodules_text(
+    gitmodules_text: str,
+    mod_name: str,
+    new_branch: str,
+) -> str:
+    mod_branch_re = re.compile(
+        fr"(\[submodule \"{mod_name}\"](\s[^\[]+)+branch\s?=\s?)(.+)",
+    )
     # check if submodule currently has a branch
     if mod_branch_re.match(gitmodules_text):
         # if it currently has a branch, update branch
         return mod_branch_re.sub(
-            fr"\g<1>{new_branch}",
+            fr'\g<1>{new_branch}',
             gitmodules_text,
         )
     else:
         # if it doesn't have a branch, add branch
-        linebreak = "\r\n" if "\r\n" in gitmodules_text else "\n"
+        linebreak = '\r\n' if '\r\n' in gitmodules_text else '\n'
         return re.sub(
             fr"(\[submodule \"{mod_name}\"])",
-            fr"\g<1>{linebreak}\tbranch = {new_branch}",
+            fr'\g<1>{linebreak}\tbranch = {new_branch}',
             gitmodules_text,
         )
 
 
-def main(argv: Optional[Sequence[str]] = None) -> int:
+def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--allow-unset", action="store_true")
-    parser.add_argument("--update-gitmodules-file", action="store_true")
-    parser.add_argument("filenames", nargs="*")
+    parser.add_argument('--allow-unset', action='store_true')
+    parser.add_argument('--update-gitmodules-file', action='store_true')
+    parser.add_argument('filenames', nargs='*')
     args = parser.parse_args(argv)
 
-    from_ref = os.environ.get("PRE_COMMIT_FROM_REF")
-    to_ref = os.environ.get("PRE_COMMIT_TO_REF")
+    from_ref = os.environ.get('PRE_COMMIT_FROM_REF')
+    to_ref = os.environ.get('PRE_COMMIT_TO_REF')
 
-    gitmodules_path = Path(".gitmodules")
+    gitmodules_path = Path('.gitmodules')
     gitmodules_text = None
     gitmodules_properties = None
     if gitmodules_path.exists():
@@ -118,53 +136,75 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     retv = 0
     gitmodules_text_changed = False
     for diff_line in diff_data:
-        if diff_line.mode_dst == "160000":
-            # It"s a submodule
+        if diff_line.mode_dst == '160000':
+            # It's a submodule
             submodule_path = diff_line.src
-            if diff_line.status in ("C", "R"):
+            if diff_line.status in ('C', 'R') and diff_line.dst:
                 # For copy and rename we check the destination
                 submodule_path = diff_line.dst
 
             submodule_name = None
-            submodule_props: Optional[Dict[str, str]] = None
+            submodule_props: dict[str, str] | None = None
             if gitmodules_properties:
-                for name, props in gitmodules_properties:
-                    if submodule_path == props["path"]:
+                for name, props in gitmodules_properties.items():
+                    if submodule_path == props['path']:
                         submodule_name = name
                         submodule_props = props
                         break
-            props_branch = submodule_props.get("branch", None)
+            props_branch = None
+            if submodule_props and 'branch' in submodule_props:
+                props_branch = submodule_props['branch']
             if not args.allow_unset:
                 if not submodule_props:
                     retv += 1
                     print(
-                        f"No submodule `{submodule_path}` found in `.gitmodules`. "
-                        f"This happens when a submodule is added with `git add` rather "
-                        f"than with the `git submodule` command."
+                        f'No submodule `{submodule_path}` found in '
+                        f'`.gitmodules`. This happens when a submodule '
+                        f'is added with `git add` rather than with the '
+                        f'`git submodule` command.',
                     )
                 elif not props_branch:
                     retv += 1
-                    print(f"Property `branch` unset for submodule `{submodule_name}`.")
+                    print(
+                        f'Property `branch` unset for submodule '
+                        f'`{submodule_name}`.',
+                    )
 
             branch_prop_needs_update = False
             if props_branch:
-                on_branch_out = cmd_output("git", "branch", props_branch, "--contains", diff_line.sha1_dst)
+                on_branch_out = cmd_output(
+                    'git',
+                    'branch',
+                    props_branch,
+                    '--contains',
+                    diff_line.sha1_dst,
+                )
                 if props_branch not in on_branch_out:
                     retv += 1
                     branch_prop_needs_update = True
-                    print(f"The submodule commit is not part of the configured branch `{props_branch}`.")
+                    print(
+                        f'The submodule commit is not part of the configured '
+                        f'branch `{props_branch}`.',
+                    )
             else:
                 branch_prop_needs_update = True
 
             if args.update_gitmodules_file and branch_prop_needs_update:
-                abbrev_ref_out = cmd_output("git", "rev-parse", "--abbrev-ref", "HEAD", cwd=submodule_path)
-                if abbrev_ref_out == "HEAD":
+                abbrev_ref_out = cmd_output(
+                    'git',
+                    'rev-parse',
+                    '--abbrev-ref',
+                    'HEAD',
+                    cwd=submodule_path,
+                )
+                if abbrev_ref_out == 'HEAD':
                     if not props_branch and not args.allow_unset:
                         print(
-                            f"Submodule `{submodule_name}` has a detached HEAD "
-                            f"unable to retrieve branch name to update `.gitmodules` file."
+                            f'Submodule `{submodule_name}` has a detached '
+                            f'HEAD unable to retrieve branch name to update '
+                            f'`.gitmodules` file.',
                         )
-                else:
+                elif gitmodules_text and submodule_name:
                     gitmodules_text_changed = True
                     gitmodules_text = update_branch_prop_in_gitmodules_text(
                         gitmodules_text=gitmodules_text,
@@ -172,11 +212,11 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
                         new_branch=abbrev_ref_out,
                     )
 
-    if gitmodules_text_changed:
+    if gitmodules_text_changed and gitmodules_text is not None:
         gitmodules_path.write_text(gitmodules_text)
 
     return retv
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     raise SystemExit(main())
